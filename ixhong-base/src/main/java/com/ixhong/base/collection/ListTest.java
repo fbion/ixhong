9510537daaeac5e4d2b68320f58f788b3ca829ae
沈红喜 <shenhongxi@haodai.com>
2015-07-24 18:10:36 +0800

    单例与序列化和反射

---
+++ ixhong-base/src/main/java/com/ixhong/base/collection/ListTest.java
@@ -0,0 +1,14 @@
+package com.ixhong.base.collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by shenhongxi on 15/7/20.
+ */
+public class ListTest {
+
+    public static void main(String[] args) {
+        Queue<String> que = new LinkedList<String>();
+    }
+}
